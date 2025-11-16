b1b9d3a084115abf038fae096578ede024a69172
Piotr Kawa <piotrkawa95@gmail.com>
2025-11-16 15:14:19 +0100

    first commit

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open("pwa-cache").then(cache => {
+            return cache.addAll([
+                "./",
+                "index.html",
+                "manifest.json",
+                "logos/logo.png",
+                "logos/icon.png"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(resp => resp || fetch(event.request))
+    );
+});
